bf620ec5423c1ed9a019b5f2555d0f92300dfab8
Adél Juhasz <sz0juhade@vasvari.org>
2022-04-01 21:31:50 +0200

    Tesztadatok feltöltése

---
+++ EventExplorer.Api/Migrations/20220401192748_InitialData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventExplorer.Api.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO categories () VALUES ();");
+            migrationBuilder.Sql("INSERT INTO events () VALUES ();");
+            migrationBuilder.Sql("INSERT INTO locations () VALUES ();");
+            migrationBuilder.Sql("INSERT INTO organizers () VALUES ();");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM categories WHERE ;");
+            migrationBuilder.Sql("DELETE FROM events WHERE ;");
+            migrationBuilder.Sql("DELETE FROM locations WHERE ;");
+            migrationBuilder.Sql("DELETE FROM organizers WHERE ;");
+        }
+    }
+}
