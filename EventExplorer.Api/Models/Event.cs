6c6b701eda28cbe1ba5a67603c317f6272dd16e9
Adél Juhasz <sz0juhade@vasvari.org>
2022-04-01 19:08:07 +0200

    Rendezvény lemodellezése

---
+++ EventExplorer.Api/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EventExplorer.Api.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MaximumHeadCount { get; set; }
+        public bool IsIndoor { get; set; }
+        public bool NeedCovidCertificate { get; set; }
+    }
+}
