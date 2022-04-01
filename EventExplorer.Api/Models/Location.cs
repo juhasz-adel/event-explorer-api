6b17b4aece0c2fbd61c5b257629abf54b76c66d6
Adél Juhasz <sz0juhade@vasvari.org>
2022-04-01 19:12:44 +0200

    Helyszín lemodellezése

---
+++ EventExplorer.Api/Models/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace EventExplorer.Api.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
