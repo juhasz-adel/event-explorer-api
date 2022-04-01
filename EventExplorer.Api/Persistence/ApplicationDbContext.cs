9d75a3b4fc69ba3424861f17fcbac77b5b11b875
juhasz-adel <77925581+juhasz-adel@users.noreply.github.com>
2022-04-01 19:39:46 +0200

    EntityFrameworkCore konfigurálás (#3)
    * 3rd party library-k telepítése
    
    * DbContext konfiguráció

---
+++ EventExplorer.Api/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventExplorer.Api.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> contextOptions)
+            : base(contextOptions)
+        {
+        }
+    }
+}
