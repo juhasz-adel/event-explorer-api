e10014f3ee5c9771458f69fc3fe850ea2527cb36
Adél Juhasz <sz0juhade@vasvari.org>
2022-04-15 21:43:20 +0200

    Category POST végpont

---
+++ EventExplorer.Api/Controllers/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EventExplorer.Api.Controllers.Resources.Requests;
+using EventExplorer.Api.Controllers.Resources.Responses;
+using EventExplorer.Api.Models;
+
+namespace EventExplorer.Api.Controllers.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Model to response resource
+            CreateMap<Category, CategoryResponseResource>();
+
+            // Request resource to model
+            CreateMap<CreateCategoryRequestResource, Category>();
+        }
+    }
+}
