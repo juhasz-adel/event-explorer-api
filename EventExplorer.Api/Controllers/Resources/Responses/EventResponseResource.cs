5177d893dda9a5a79b5349d88bf8736727dcf979
Adél Juhasz <sz0juhade@vasvari.org>
2022-04-15 18:43:45 +0200

    GET végpontok implementálása minden modelre

---
+++ EventExplorer.Api/Controllers/Resources/Responses/EventResponseResource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EventExplorer.Api.Controllers.Resources.Responses
+{
+    public class EventResponseResource
+    {
+        public int Id { get; set; }
+        public OrganizerResponseResource Organizer { get; set; }
+        public CategoryResponseResource Category { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LocationResponseResource Location { get; set; }
+        public int EntranceFee { get; set; }
+        public bool IsIndoor { get; set; }
+    }
+}
---
+++ EventExplorer.Api/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using EventExplorer.Api.Controllers.Resources.Responses;
+using EventExplorer.Api.Models;
+using EventExplorer.Api.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventExplorer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UsersController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users =
+                _context.Users.ToList();
+
+            var userResponseResources =
+                _mapper.Map<IEnumerable<User>, IEnumerable<UserResponseResource>>(users);
+
+            return Ok(userResponseResources);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetUser(int id)
+        {
+            var user =
+                _context.Users
+                .SingleOrDefault(e => e.Id == id);
+
+            if (user == null)
+            {
+                return NotFound("User not found with id: " + id);
+            }
+
+            var userResponseResource =
+                _mapper.Map<User, UserResponseResource>(user);
+
+            return Ok(userResponseResource);
+        }
+    }
+}
