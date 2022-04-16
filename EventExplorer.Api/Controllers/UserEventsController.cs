c64708c7008a5dd136b9022ed0fbfd2e4d1288ef
Adél Juhasz <sz0juhade@vasvari.org>
2022-04-16 19:06:14 +0200

    User events végpont

---
+++ EventExplorer.Api/Controllers/UserEventsController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using EventExplorer.Api.Controllers.Resources.Responses;
+using EventExplorer.Api.Models;
+using EventExplorer.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace EventExplorer.Api.Controllers
+{
+    [ApiController]
+    [Route("api/user/{userId:int}/events/")]
+    public class UserEventsController : ControllerBase
+    {
+        private readonly AttendanceService _attendanceService;
+        private readonly IMapper _mapper;
+
+        public UserEventsController(
+            AttendanceService attendanceService,
+            IMapper mapper
+        )
+        {
+            _attendanceService = attendanceService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetUserEvents(int userId)
+        {
+            try
+            {
+                var events =
+                    _attendanceService.GetUserEvents(userId);
+
+                var eventResponseResources =
+                    _mapper.Map<IEnumerable<Event>, IEnumerable<EventResponseResource>>(events);
+
+                return Ok(eventResponseResources);
+            }
+            catch (Exception exception)
+            {
+                return NotFound(exception.Message);
+            }
+        }
+    }
+}
