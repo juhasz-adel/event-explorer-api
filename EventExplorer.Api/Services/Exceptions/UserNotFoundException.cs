74081e53d0844f8464c34d6d4526ea79b059f553
Adél Juhasz <sz0juhade@vasvari.org>
2022-04-16 15:32:31 +0200

    UserService

---
+++ EventExplorer.Api/Services/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventExplorer.Api.Services.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException(int userId)
+            : base($"Felhasználó nem található a következő id-vel: {userId}")
+        {
+        }
+    }
+}
